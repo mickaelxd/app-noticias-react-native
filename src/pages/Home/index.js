ea5512698f7d4d7ceb26d6c1b2fa532cd6d8d89f
Mickael <mickael.n.xd@gmail.com>
2021-04-06 19:52:58 -0300

    Tela mostrando as noticias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { isEmpty } from 'lodash';
+
+import TextLabel from '~/components/TextLabel';
 import Button from '~/components/Button';
 
-const Home = () => {
-  const navigation = useNavigation();
+import { Container } from './styles';
+
+const Home = ({ route, navigation }) => {
+  const [news, setNews] = useState([]);
+
+  const handleNews = useCallback(async () => {
+    try {
+      if (route.params?.object) {
+        if (isEmpty(route.params?.object)) return;
+        setNews([route.params?.object, ...news]);
+      }
+    } catch (e) {
+      // console.log(e);
+    }
+  }, [news, route.params?.object]);
+
+  useEffect(() => {
+    if (route.params?.reload) {
+      handleNews();
+      navigation.setParams({ reload: false, object: {} });
+    }
+  }, [handleNews, navigation, route.params]);
+
+  useEffect(() => handleNews(), []);
 
   return (
-    <View>
+    <Container>
       <Button
         title="Criar Nova Noticia"
         onPress={() => navigation.navigate('CreateNews')}
       />
-    </View>
+      {news?.map(NewsItem => (
+        <React.Fragment key={NewsItem.title}>
+          <TextLabel color="black" type="bold" marginTop={16}>
+            {NewsItem.title}
+          </TextLabel>
+          <TextLabel color="black">{NewsItem.body}</TextLabel>
+        </React.Fragment>
+      ))}
+    </Container>
   );
 };
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin: 17px 24px;
+  flex: 1;
+`;
