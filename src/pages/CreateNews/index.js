ac8cf073c9a85cbd3d0411b34e3a1ad0bc185a77
Mickael <mickael.n.xd@gmail.com>
2021-04-06 19:36:47 -0300

    Criado tela de Criação de Noticia

--- src/pages/CreateNews/index.js
+++ src/pages/CreateNews/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+
+import { Container } from './styles';
 
 const CreateNews = () => {
+  const [titleValue, setTitleValue] = useState(null);
+  const [bodyValue, setBodyValue] = useState(null);
+  const navigation = useNavigation();
+
+  const handleContinue = () => {
+    if (
+      titleValue &&
+      titleValue.length > 10 &&
+      bodyValue &&
+      bodyValue.length > 10
+    ) {
+      const object = {
+        title: titleValue,
+        body: bodyValue,
+      };
+
+      navigation.navigate('Home', { object, reload: true });
+    }
+  };
+
   return (
-    <View>
-      <Text>Tela 1</Text>
-    </View>
+    <Container>
+      <Input
+        onChangeText={text => setTitleValue(text)}
+        value={titleValue}
+        marginBottom="15px"
+        label="Título da Notícia"
+      />
+      <Input
+        onChangeText={text => setBodyValue(text)}
+        value={bodyValue}
+        marginBottom="15px"
+        label="Corpo da Notícia"
+        multiline
+      />
+      <Button title="Salvar" onPress={handleContinue} />
+    </Container>
   );
 };
 
---
+++ src/pages/CreateNews/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  margin: 17px 24px;
+`;
