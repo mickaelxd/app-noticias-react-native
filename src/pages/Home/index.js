589c50bf129a661b983d1db38d9b1c7c14ba6c1f
Mickael <mickael.n.xd@gmail.com>
2021-04-07 07:13:14 -0300

    Filtro de Noticias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { isEmpty } from 'lodash';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 import TextLabel from '~/components/TextLabel';
 import Button from '~/components/Button';
 
-import { Container } from './styles';
+import { Container, IconContainer } from './styles';
+import Input from '~/components/Input';
+import { CONSTANT_NEWS } from '~/utils/constantNews';
+import Card from '~/components/Card';
 
 const Home = ({ route, navigation }) => {
-  const [news, setNews] = useState([]);
+  const [news, setNews] = useState(CONSTANT_NEWS);
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const [searchBarText, setSearchBarText] = useState('');
 
   const handleNews = useCallback(async () => {
     try {
@@ -16,32 +22,70 @@ const Home = ({ route, navigation }) => {
         setNews([route.params?.object, ...news]);
       }
     } catch (e) {
-      // console.log(e);
+      // eslint-disable-next-line no-console
+      console.log(e);
     }
   }, [news, route.params?.object]);
 
+  const handleEditNews = useCallback(() => {}, []);
+
+  const filteredNews = useCallback(() => {
+    if (searchBarText) {
+      return news.filter(newsItem =>
+        newsItem.title.toLowerCase().includes(searchBarText.toLowerCase())
+      );
+    }
+
+    return news;
+  }, [news, searchBarText]);
+
+  // After Create News, reload News Object
   useEffect(() => {
     if (route.params?.reload) {
       handleNews();
       navigation.setParams({ reload: false, object: {} });
     }
   }, [handleNews, navigation, route.params]);
-
-  useEffect(() => handleNews(), []);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <IconContainer
+          onPress={() => setShowSearchBar(prevState => !prevState)}
+        >
+          {!showSearchBar ? (
+            <Icon name="search" size={24} color="black" />
+          ) : (
+            <Icon name="pencil" size={24} color="black" />
+          )}
+        </IconContainer>
+      ),
+    });
+    handleNews();
+  }, [handleNews, navigation, showSearchBar]);
 
   return (
     <Container>
-      <Button
-        title="Criar Nova Noticia"
-        onPress={() => navigation.navigate('CreateNews')}
-      />
-      {news?.map(NewsItem => (
-        <React.Fragment key={NewsItem.title}>
-          <TextLabel color="black" type="bold" marginTop={16}>
+      {showSearchBar ? (
+        <Input
+          label="Pesquisar Notícia por Título"
+          value={searchBarText}
+          onChangeText={text => setSearchBarText(text)}
+        />
+      ) : (
+        <Button
+          title="Criar Nova Noticia"
+          onPress={() => navigation.navigate('CreateNews')}
+        />
+      )}
+
+      {filteredNews()?.map(NewsItem => (
+        <Card key={NewsItem.title} onPress={() => handleEditNews(NewsItem)}>
+          <TextLabel color="black" type="bold">
             {NewsItem.title}
           </TextLabel>
           <TextLabel color="black">{NewsItem.body}</TextLabel>
-        </React.Fragment>
+          <TextLabel color="grey">Autor: {NewsItem.author}</TextLabel>
+        </Card>
       ))}
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,3 +4,8 @@ export const Container = styled.View`
   margin: 17px 24px;
   flex: 1;
 `;
+
+export const IconContainer = styled.TouchableOpacity`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
